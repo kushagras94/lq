1a6e6d54d8ca13afa3a6f244f5fb5664e770c225
kushagras94 <88883214+kushagras94@users.noreply.github.com>
2025-11-29 18:03:18 +0530

    Update greeting message in Flask app

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__)  # <-- this *name* matters; Vercel looks for `app`
+# Vercel looks specifically for this name: `app`
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Hello from Flask on Vercel"})
+    return jsonify({"message": "Hello from Flask on Vercel!"})
